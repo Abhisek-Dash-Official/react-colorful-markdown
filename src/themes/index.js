e58e2ecc768fb97d1a686168532cc515b206ffc3
ABHISEK DASH <im.abhisekdash.9753@gmail.com>
2026-07-26 17:39:32 +0530

    feat: register comprehensive color palette in theme map

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,8 +1,64 @@
-// import { yellowTheme } from "./yellow";
-// import { blueTheme } from "./blue";
+import { redTheme } from "./red";
+import { orangeTheme } from "./orange";
+import { amberTheme } from "./amber";
+import { yellowTheme } from "./yellow";
+import { limeTheme } from "./lime";
+import { greenTheme } from "./green";
+import { emeraldTheme } from "./emerald";
+import { tealTheme } from "./teal";
+import { cyanTheme } from "./cyan";
+import { skyTheme } from "./sky";
+import { blueTheme } from "./blue";
+import { indigoTheme } from "./indigo";
+import { violetTheme } from "./violet";
+import { purpleTheme } from "./purple";
+import { fuchsiaTheme } from "./fuchsia";
+import { pinkTheme } from "./pink";
+import { roseTheme } from "./rose";
+import { rubyTheme } from "./ruby";
+import { crimsonTheme } from "./crimson";
+import { coralTheme } from "./coral";
+import { goldTheme } from "./gold";
+import { mintTheme } from "./mint";
+import { sapphireTheme } from "./sapphire";
+import { amethystTheme } from "./amethyst";
+import { lavenderTheme } from "./lavender";
+import { slateTheme } from "./slate";
+import { grayTheme } from "./gray";
+import { zincTheme } from "./zinc";
+import { neutralTheme } from "./neutral";
+import { stoneTheme } from "./stone";
 
 export const themeMap = {
-    // "yellow": yellowTheme,
-    // "blue": blueTheme,
-    // default: blueTheme, // Fallback
+    "red": redTheme,
+    "orange": orangeTheme,
+    "amber": amberTheme,
+    "yellow": yellowTheme,
+    "lime": limeTheme,
+    "green": greenTheme,
+    "emerald": emeraldTheme,
+    "teal": tealTheme,
+    "cyan": cyanTheme,
+    "sky": skyTheme,
+    "blue": blueTheme,
+    "indigo": indigoTheme,
+    "violet": violetTheme,
+    "purple": purpleTheme,
+    "fuchsia": fuchsiaTheme,
+    "pink": pinkTheme,
+    "rose": roseTheme,
+    "ruby": rubyTheme,
+    "crimson": crimsonTheme,
+    "coral": coralTheme,
+    "gold": goldTheme,
+    "mint": mintTheme,
+    "sapphire": sapphireTheme,
+    "amethyst": amethystTheme,
+    "lavender": lavenderTheme,
+    "slate": slateTheme,
+    "gray": grayTheme,
+    "zinc": zincTheme,
+    "neutral": neutralTheme,
+    "stone": stoneTheme,
+    "default": slateTheme
 };
